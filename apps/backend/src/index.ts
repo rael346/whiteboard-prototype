8544c21abeba66c6b1215bbc4640bbd513fd17b5
Duy Tran <duyvutran2303@gmail.com>
2023-10-14 18:17:10 -0400

    chore: init backend

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { Server as SocketServer } from "socket.io";
+
+const PORT = 4000;
+const app = express();
+const server = app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`);
+});
+
+app.get("/helloworld", (req, res) => {
+  res.send("Hello World");
+});
+
+const io = new SocketServer(server);
+
+io.on("connection", socket => {
+  console.log("Made socket connection");
+
+  socket.on("hello", data => {
+    console.log(data);
+    io.emit("hello");
+  });
+});
